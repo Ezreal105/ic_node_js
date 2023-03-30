8653b2c82836605dc967f6e7ea2fcbe93760582c
wangshoutai <shoutai.wang@lotuscars.com.cn>
2023-03-30 15:30:15 +0800

    unconmment WIN32

--- src/binding.cc
+++ src/binding.cc
@@ -1,56 +1,56 @@
 #ifdef _WIN32
 #include <napi.h>
 #include <node_api.h>
-// #include <Windows.h>
-// #include <libloaderapi.h>
+#include <Windows.h>
+#include <libloaderapi.h>
 
-// bool loadLibrary(const char* dllPath, HMODULE& module) {
-//     module = LoadLibraryA(dllPath);
-//     if (module == NULL) {
-//         return false;
-//     }
-//     return true;
-// }
+bool loadLibrary(const char* dllPath, HMODULE& module) {
+    module = LoadLibraryA(dllPath);
+    if (module == NULL) {
+        return false;
+    }
+    return true;
+}
 
-// bool freeLibrary(HMODULE& module) {
-//     if (module != NULL) {
-//         FreeLibrary(module);
-//         module = NULL;
-//         return true;
-//     }
-//     return false;
-// }
+bool freeLibrary(HMODULE& module) {
+    if (module != NULL) {
+        FreeLibrary(module);
+        module = NULL;
+        return true;
+    }
+    return false;
+}
 
-// typedef int(*GetDeviceCountFunction)();
-
-// Napi::Value IC_GetDeviceCount(const Napi::CallbackInfo& info) {
-//     Napi::Env env = info.Env();
-
-//     HMODULE module;
-//     if (!loadLibrary("tisgrabber_x64.dll", module)) {
-//         Napi::TypeError::New(env, "Failed to load dll").ThrowAsJavaScriptException();
-//         return env.Null();
-//     }
-
-//     GetDeviceCountFunction getDeviceCountFunction = (GetDeviceCountFunction)GetProcAddress(module, "IC_GetDeviceCount");
-//     if (getDeviceCountFunction == NULL) {
-//         Napi::TypeError::New(env, "Failed to get function").ThrowAsJavaScriptException();
-//         freeLibrary(module);
-//         return env.Null();
-//     }
-
-//     int result = getDeviceCountFunction();
-
-//     freeLibrary(module);
-
-//     return Napi::Number::New(env, result);
-// }
+typedef int(*GetDeviceCountFunction)();
 
 Napi::Value IC_GetDeviceCount(const Napi::CallbackInfo& info) {
     Napi::Env env = info.Env();
 
-    return Napi::Number::New(env, -1);
+    HMODULE module;
+    if (!loadLibrary("tisgrabber_x64.dll", module)) {
+        Napi::TypeError::New(env, "Failed to load dll").ThrowAsJavaScriptException();
+        return env.Null();
+    }
+
+    GetDeviceCountFunction getDeviceCountFunction = (GetDeviceCountFunction)GetProcAddress(module, "IC_GetDeviceCount");
+    if (getDeviceCountFunction == NULL) {
+        Napi::TypeError::New(env, "Failed to get function").ThrowAsJavaScriptException();
+        freeLibrary(module);
+        return env.Null();
+    }
+
+    int result = getDeviceCountFunction();
+
+    freeLibrary(module);
+
+    return Napi::Number::New(env, result);
 }
+
+// Napi::Value IC_GetDeviceCount(const Napi::CallbackInfo& info) {
+//     Napi::Env env = info.Env();
+
+//     return Napi::Number::New(env, -1);
+// }
 
 Napi::Object Init(Napi::Env env, Napi::Object exports) {
     exports.Set("IC_GetDeviceCount", Napi::Function::New(env, IC_GetDeviceCount));
