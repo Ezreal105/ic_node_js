9453827c8f5e8af0a9301ebe42dd0a55dcf9eace
wangshoutai <shoutai.wang@lotuscars.com.cn>
2023-03-30 12:48:20 +0800

    14

--- build.cmd
+++ build.cmd
@@ -1,7 +1,7 @@
 echo building...
-set GYP_MSVS_VERSION=2019 &^
+set GYP_MSVS_VERSION=2019 &&^
 npm install &&^
 npm run build:ci &&^
 npm run package:ci &&^
-echo %node_pre_gyp_github_token%
-set NODE_PRE_GYP_GITHUB_TOKEN=%node_pre_gyp_github_token% npm run publish:ci
+set NODE_PRE_GYP_GITHUB_TOKEN=%node_pre_gyp_github_token% &&^
+npm run publish:ci
