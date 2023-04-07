46de49d2aa174d74d55f4f1fccd5bfd42a196c8b
wangshoutai <shoutai.wang@lotuscars.com.cn>
2023-04-07 16:07:22 +0800

    chore(build): copy dll to dist

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,14 +1,15 @@
-import typescript from "@rollup/plugin-typescript";
-import pkg from "./package.json" assert { type: "json" };
+import typescript from '@rollup/plugin-typescript';
+import copy from 'rollup-plugin-copy';
+import pkg from './package.json' assert {type: 'json'};
 
 export default [
   {
-    input: "src/index.ts",
-    plugins: [typescript()],
-    external: ["ms"],
+    input: 'src/index.ts',
+    plugins: [typescript(), copy({targets: [{src: 'src/*.dll', dest: 'dist'}]})],
+    external: ['ms'],
     output: [
-      { file: pkg.main, format: "cjs" },
-      { file: pkg.module, format: "es" },
+      {file: pkg.main, format: 'cjs'},
+      {file: pkg.module, format: 'es'},
     ],
   },
 ];
