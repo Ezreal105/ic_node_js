4b10b77b35d29c206d9d89c4eb5c3582d1a41020
wangshoutai <shoutai.wang@lotuscars.com.cn>
2023-05-06 16:26:04 +0800

    feat: add IC_ReleaseGrabber

--- src/binding.cc
+++ src/binding.cc
@@ -216,7 +216,7 @@ Napi::Value f_IC_ReleaseGrabber(const Napi::CallbackInfo &info)
         return env.Undefined();
     };
     auto f = *fPtr;
-    f();
+    f(hGrabber);
     Napi::Object retObj = Napi::Object::New(env);
     retObj.Set("code", Napi::Number::New(env, IC_SUCCESS));
     return retObj;
